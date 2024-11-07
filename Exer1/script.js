4c9e4fc43dd4fbffe31c00d3cd411b8519e71926
raffimolero <49224759+raffimolero@users.noreply.github.com>
2024-11-07 08:41:56 +0800

    More descriptive error

--- Exer1/script.js
+++ Exer1/script.js
@@ -3,27 +3,26 @@ function validateWith(requirements) {
   for (const [key, value] of Object.entries(requirements)) {
     const isValid = value.regex.test(document.getElementById(key).value);
     if (!isValid) {
-      err += `Invalid ${key}. ${value.error}\n`;
+      err += `Invalid ${key}: ${value.error}\n`;
     }
   }
-  if (err === '') {
-    return true;
-  } else {
-    alert(err);
-    return false;
-  }
+  return err;
 }
 
 function validate() {
-  const isValid = validateWith({
+  const err = validateWith({
     name: {
       regex: /[A-Z][a-z]*, ([A-Z][a-z]* )+[A-Z]\./,
       error: "Must follow the format Lastname, Firstname M.",
     },
     email: {
       regex: /[\w\.]+@\w+(?:\.\w+)+/,
-      error: "Must be a valid email such as johndoe@example.com.",
+      error: "Must be a valid email like name@example.com",
     },
   });
+  if (err === '') {
+    alert('Registered.');
+  } else {
+    alert('Failed to register:\n\n' + err);
+  }
 }
-
